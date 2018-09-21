99624c83e1488e891c8a29a9642f6421467f5e7f
Arshpreet Kaur <arshpreetkaur989@gmail.com>
2018-09-21 09:24:39 -0500

    Initial Commit

---
+++ Assignment1/Models/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Assignment1.Models
+{
+    public class Car
+    {
+        public int Cost { get; set; }
+        public int Year { get; set; }
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public string Color { get; set; }
+    }
+}
+
